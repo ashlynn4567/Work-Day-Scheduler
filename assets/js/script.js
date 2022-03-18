78ca3edd492a60ac8653bf18907db474cfb8cfb1
Ashley Smith <ashlynn4567@gmail.com>
2022-03-18 10:26:34 -0500

    added dynamic color change and styling to timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,38 @@
 // using moment.js to display current time and date
-// check current time 
 setInterval(function() {
     // display the current day
-    $("#currentDay").text(moment());
-}, 1000);
-
-
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY LTS"));
+}, 0);
 
 
 // displaying the time blocks
 var displayTimeBlocks = function() {
-    timeBlockArray = ["0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700"];
+    timeBlockArray = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
     
     // for each time block 
     for (i = 0; i < timeBlockArray.length; i++) {
-        timeBlockId = timeBlockArray[i];
+        var timeBlockId = timeBlockArray[i];
 
         // create timeblock child div
         var timeBlockChild = $(`<div id='${timeBlockId}' class='row time-block'></div>`)
         // convert military time to display variable
-        convertedTime = moment(timeBlockArray[i], "HHmm").format("hh A");
+        convertedTime = moment(timeBlockArray[i], "H").format("h A");
         console.log(convertedTime);
         
         // create hour div, textarea, and button elements
-        var timeBlockHour = $(`<div class='hour'>${convertedTime}</div>`);
-        var timeBlockTextArea = $("<textarea class='description'></textarea>");
-        var timeBlockButton = $("<button class='saveBtn btn btn-success'>Save</button>")
-        
+        var timeBlockHour = $(`<div class='hour col-md-1'>${convertedTime}</div>`);
+        var timeBlockTextArea = $("<textarea class='description col-md-10'></textarea>");
+        var timeBlockButton = $("<button class='saveBtn btn btn-success col-md-1 d-flex align-items-center justify-content-center'></button>")
+        var timeBlockIcon = $("<i class='fas fa-save'></i>");
+
         // append hour div, textarea, and button elements to timeblockchild
         timeBlockChild.append(timeBlockHour);
         timeBlockChild.append(timeBlockTextArea);
+        timeBlockButton.append(timeBlockIcon);
         timeBlockChild.append(timeBlockButton);
+
+        // audit time block with color
+        colorTimeBlocks(timeBlockId, timeBlockTextArea);
 
         // find container element
         var timeBlockContainerEl = $(".container");
@@ -40,20 +42,32 @@ var displayTimeBlocks = function() {
 };
 
 
+// compare current time to timeblock time
+var colorTimeBlocks = function(timeLabel, timeBG) {   
+    // convert timeblockid to momentjs time format
+    var currentTime = moment().hour();
+    
+    // if timeblock is less than current time
+    if (timeLabel < currentTime) {
+        // set color to grey for past event
+        timeBG.addClass("past");
+        timeBG.removeClass("present");
+        timeBG.removeClass("future");
 
-
-var colorTimeBlocks = function() {
-    // compare current time to timeblock time
-    moment("09:00", 'HH:mm')
-
-        // if timeblock is less than current time
-            // set color to grey for past event
-
-        // if timeblock is equal to current time
-            // set color to red for present event
-
-        // if timeblock is greater than current time
-            // set color to green for future event
+    // if timeblock is equal to current time
+    } else if (timeLabel == currentTime) {
+        // set color to red for present event
+        timeBG.addClass("present");
+        timeBG.removeClass("past");
+        timeBG.removeClass("future");
+    
+    // if timeblock is greater than current time
+    } else if (timeLabel > currentTime) {
+        // set color to green for future event
+        timeBG.addClass("future");
+        timeBG.removeClass("past");
+        timeBG.removeClass("present");
+    };            
 };
 
 
@@ -79,4 +93,3 @@ var colorTimeBlocks = function() {
 
 // call all display functions
 displayTimeBlocks();
-colorTimeBlocks();
